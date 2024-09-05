f0b8847992f8099331c7037699383b572cfa2299
zenglu <93021236+17679267885@users.noreply.github.com>
2024-09-05 10:00:13 +0800

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,132 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+
+router.get('/', (req, res, next) => {
+	return res.render('index.ejs');
+});
+
+
+router.post('/', (req, res, next) => {
+	let personInfo = req.body;
+
+	if (!personInfo.email || !personInfo.username || !personInfo.password || !personInfo.passwordConf) {
+		res.send();
+	} else {
+		if (personInfo.password == personInfo.passwordConf) {
+
+			User.findOne({ email: personInfo.email }, (err, data) => {
+				if (!data) {
+					let c;
+					User.findOne({}, (err, data) => {
+
+						if (data) {
+							c = data.unique_id + 1;
+						} else {
+							c = 1;
+						}
+
+						let newPerson = new User({
+							unique_id: c,
+							email: personInfo.email,
+							username: personInfo.username,
+							password: personInfo.password,
+							passwordConf: personInfo.passwordConf
+						});
+
+						newPerson.save((err, Person) => {
+							if (err)
+								console.log(err);
+							else
+								console.log('Success');
+						});
+
+					}).sort({ _id: -1 }).limit(1);
+					res.send({ "Success": "您已注册,现在可以登录了." });
+				} else {
+					res.send({ "Success": "邮件已经被使用了。" });
+				}
+
+			});
+		} else {
+			res.send({ "Success": "密码不匹配" });
+		}
+	}
+});
+
+router.get('/login', (req, res, next) => {
+	return res.render('login.ejs');
+});
+
+router.post('/login', (req, res, next) => {
+	User.findOne({ email: req.body.email }, (err, data) => {
+		if (data) {
+
+			if (data.password == req.body.password) {
+				req.session.userId = data.unique_id;
+				res.send({code:200, "Success": "成功！" });
+			} else {
+				res.send({ "Success": "密码错误！" });
+			}
+		} else {
+			res.send({ "Success": "邮件未注册!" });
+		}
+	});
+});
+
+router.get('/profile', (req, res, next) => {
+	User.findOne({ unique_id: req.session.userId }, (err, data) => {
+		if (!data) {
+			res.redirect('/');
+		} else {
+			return res.render('data.ejs', { "name": data.username, "email": data.email });
+		}
+	});
+});
+
+router.get('/logout', (req, res, next) => {
+	if (req.session) {
+		// delete session object
+		req.session.destroy((err) => {
+			if (err) {
+				return next(err);
+			} else {
+				return res.redirect('/');
+			}
+		});
+	}
+});
+
+router.get('/forgetpass', (req, res, next) => {
+	res.render("forget.ejs");
+});
+
+router.post('/forgetpass', (req, res, next) => {
+	User.findOne({ email: req.body.email }, (err, data) => {
+		if (!data) {
+			res.send({ "Success": "This Email Is not regestered!" });
+		} else {
+			if (req.body.password == req.body.passwordConf) {
+				data.password = req.body.password;
+				data.passwordConf = req.body.passwordConf;
+
+				data.save((err, Person) => {
+					if (err)
+						console.log(err);
+					else
+						console.log('Success');
+					res.send({ "Success": "密码改变了!" });
+				});
+			} else {
+				res.send({ "Success": "密码不匹配!两个密码必须相同" });
+			}
+		}
+	});
+
+});
+router.get('/main',function(req,res){
+	res.render('main',{
+		title:'主页'
+	})
+})
+module.exports = router;
